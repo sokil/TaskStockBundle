6e28411c06b96e984641615599a397e4a46a147a
Dmytro Sokil <dmytro.sokil@gmail.com>
2016-10-08 23:02:54 +0300

    def category schema options

--- src/Resources/assets/components/taskProjectParameters/messages.ru.js
+++ src/Resources/assets/components/taskProjectParameters/messages.ru.js
@@ -4,5 +4,6 @@ i18n.addMessages({
     'task_project_parameters.desciption': 'Описание проекта',
     'task_project_parameters.notification_schema': 'Схема уведомлений',
     'task_project_parameters.no_notification_schema': 'Без уведомлений',
-    'task_project_parameters.task_category_schema': 'Схема категорий'
+    'task_project_parameters.task_category_schema': 'Схема категорий',
+    'task_project_parameters.no_category_schema': 'Без категорий'
 });
--- src/Resources/assets/components/taskProjectParameters/messages.uk.js
+++ src/Resources/assets/components/taskProjectParameters/messages.uk.js
@@ -4,5 +4,6 @@ i18n.addMessages({
     'task_project_parameters.desciption': 'Опис проекту',
     'task_project_parameters.notification_schema': 'Схема повідомлень',
     'task_project_parameters.no_notification_schema': 'Без повідомлень',
-    'task_project_parameters.task_category_schema': 'Схема категорій'
+    'task_project_parameters.task_category_schema': 'Схема категорій',
+    'task_project_parameters.no_category_schema': 'Без категорій'
 });
