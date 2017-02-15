71e1fa38c837c6e1315b994ca0cee373d4be5a70
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-02-16 00:34:53 +0200

    fix fetch default

--- src/Resources/assets/models/task/Task.js
+++ src/Resources/assets/models/task/Task.js
@@ -1,10 +1,9 @@
-var Task = Backbone.Model.extend(_.extend(
-    ModelFetchDefaultsTrait,
-    {
+var Task = Backbone.Model
+    .extend({
         urlRoot: '/tasks',
 
         hasPermission: function(permission) {
             return this.get('permissions')[permission] === true;
         }
-    }
-));
+    })
+    .extend(ModelFetchDefaultsTrait);
